95aa9de30bbbab1b2e6b4f0f6c813ed28b6352e5
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-09-20 19:53:34 +0300

    Update server: do the whole work
    Add routes and show current time in json formatt

--- docs/Lab1/server.go
+++ docs/Lab1/server.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+)
+
+type TimeStruct struct {
+	Time string `json:"time"`
+}
+
+func indexPage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Visit /time page")
+	fmt.Println("Endpoint Hit: indexPage")
+}
+
+func timePage(w http.ResponseWriter, r *http.Request) {
+	timeValue := time.Now()
+	timeData := TimeStruct{Time: timeValue.Format(time.RFC3339)}
+	json.NewEncoder(w).Encode(timeData)
+	fmt.Println("Endpoint Hit: timePage")
+}
+
+func routeRequests() {
+	http.HandleFunc("/", indexPage)
+	http.HandleFunc("/time", timePage)
+	http.ListenAndServe(":8795", nil)
+}
 
 func main() {
-  fmt.Println("Hello, Mezidia")
+	routeRequests()
 }
