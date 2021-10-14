a00ccd7a0908ee2029194280785d118d808db35f
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-10-14 11:12:09 +0300

    Update implementation.go
    Co-Authored-By: LICHKING <54878089+Dmytrenko-Roman@users.noreply.github.com>
    Co-Authored-By: Valentyn-Dominskyi <valentyn2202@gmail.com>

--- docs/Lab2/implementation.go
+++ docs/Lab2/implementation.go
@@ -1,6 +1,13 @@
 package lab2
 
-import "fmt"
+import (
+	"fmt"
+	"unicode"
+)
+
+func isDigit(symbol rune) bool {
+	return unicode.IsDigit(symbol)
+}
 
 // TODO: document this function.
 // Prefix converts
