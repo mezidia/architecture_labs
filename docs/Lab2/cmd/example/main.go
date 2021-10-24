c83a7528f43b07c7c234679931f5c279614305ae
Valentyn Dominskyi <Valentyn2202@gmail.com>
2021-10-24 14:30:53 +0300

    🚧: start cmd expressions

--- docs/Lab2/cmd/example/main.go
+++ docs/Lab2/cmd/example/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"debug/elf"
 	"flag"
 	"fmt"
+	"io"
 
 	lab2 "github.com/mezidia/architecture_labs/tree/main/docs/Lab2"
 )
@@ -13,6 +15,15 @@ import (
 // )
 
 func main() {
+	// var (
+	// 	inputExpression = flag.String("e", "", "Expression to compute")
+	// 	// TODO: Add other flags support for input and output configuration.
+	// )
+
+	inputExpression := flag.String("e", "", "Expression to compute")
+	inputFile := flag.String("f", "", "File with expression")
+	outputFile := flag.String("o", "", "File with result")
+
 	flag.Parse()
 
 	// TODO: Change this to accept input from the command line arguments as described in the task and
@@ -23,7 +34,16 @@ func main() {
 	//       }
 	//       err := handler.Compute()
 
-	res, err := lab2.Prefix("+ 11 20")
+	expressionToUse := ""
+
+	if inputExpression != nil {
+		expressionToUse = *inputExpression
+	}
+	else if inputFile != nil {
+		expressionToUse = *inputExpression
+	}
+
+	res, err := lab2.Prefix(expressionToUse)
 	if err == nil {
 		fmt.Println(res)
 	}
