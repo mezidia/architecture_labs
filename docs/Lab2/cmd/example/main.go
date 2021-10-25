2389b242b839796ab00173c33e9dcc5448ec01b4
Valentyn Dominskyi <Valentyn2202@gmail.com>
2021-10-25 17:06:49 +0300

    🚧: add smth

--- docs/Lab2/cmd/example/main.go
+++ docs/Lab2/cmd/example/main.go
@@ -16,45 +16,9 @@ import (
 // )
 
 func main() {
-	inputExpression := flag.String("e", "", "Expression to compute")
-	inputFile := flag.String("f", "", "File with expression")
-	outputFile := flag.String("o", "", "File with result")
-
-	flag.Parse()
-
-	// TODO: Change this to accept input from the command line arguments as described in the task and
-	//       output the results using the ComputeHandler instance.
-	//       handler := &lab2.ComputeHandler{
-	//           Input: {construct io.Reader according the command line parameters},
-	//           Output: {construct io.Writer according the command line parameters},
-	//       }
-	//       err := handler.Compute()
-
-	expressionToUse := ""
-	if len(*inputExpression) > 0 {
-		expressionToUse = *inputExpression
-
-	} else if len(*inputFile) > 0 {
-		input, err := os.ReadFile(*inputFile)
-
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		expressionToUse = string(input)
+	com := lab2.ComputeHandler{
+		In:  strings.NewReader("Main reader"),
+		Out: 
 	}
-
-	res, err := lab2.Prefix(expressionToUse)
-	if err == nil {
-		if len(*outputFile) > 0 {
-			resInString := strconv.Itoa(res)
-			data := []byte(resInString)
-
-			os.WriteFile(*outputFile, data, 0666)
-		} else {
-			fmt.Println(res)
-		}
-	} else {
-		fmt.Println(err)
-	}
+	com.Compute()
 }
