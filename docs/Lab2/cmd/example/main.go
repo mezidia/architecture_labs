09dd7116ca3da17cdb1a783c211c4ed9ee25af2b
Valentyn Dominskyi <Valentyn2202@gmail.com>
2021-10-28 17:42:31 +0300

    🚧: ADD SMTH WORKING

--- docs/Lab2/cmd/example/main.go
+++ docs/Lab2/cmd/example/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
+	"io"
 	"os"
 	"strings"
 
@@ -20,18 +22,39 @@ func main() {
 
 	if (*inputExpression != "" && *inputFile != "") ||
 		(*inputExpression == "" && *inputFile == "") {
-		fmt.Errorf("unexpected input")
+		fmt.Println("unexpected input")
 	} else {
 		var inputFromCMD *strings.Reader
-		var inputFromFile os.File
+		var inputFromFile io.Reader
+		var input io.Reader
+		var output io.Writer
 
 		if len(*inputExpression) > 0 {
 			inputFromCMD = strings.NewReader(*inputExpression)
+			input = inputFromCMD
+		} else {
+			inputExpressionFromFile, err := os.ReadFile(*inputFile)
+			if err != nil {
+				fmt.Println(err)
+			}
+			// convert byte slice to io.Reader
+			inputFromFile = bytes.NewReader(inputExpressionFromFile)
+			input = inputFromFile
+		}
+
+		if len(*outputFile) > 0 {
+			outputToFile, err := os.Create(*outputFile)
+			if err != nil {
+				fmt.Println(err)
+			}
+			output = outputToFile
+		} else {
+			output = os.Stdout
 		}
 
 		handler := &lab2.ComputeHandler{
-			Input:  inputFromCMD,
-			Output: os.Stdout,
+			Input:  input,
+			Output: output,
 		}
 		err := handler.Compute()
 		if err != nil {
--- docs/Lab2/handler.go
+++ docs/Lab2/handler.go
@@ -1,49 +1,33 @@
 package lab2
 
 import (
+	"bytes"
 	"fmt"
 	"io"
-	"log"
-	"os"
-	"strconv"
 )
 
-// ComputeHandler should be constructed with input io.Reader and output io.Writer.
-// Its Compute() method should read the expression from input and write the computed result to the output.
 type ComputeHandler struct {
-	// TODO: Add necessary fields.
 	Input  io.Reader
 	Output io.Writer
 }
 
 func (ch *ComputeHandler) Compute() error {
-	expressionToUse := ""
-	buf := make([]byte, 4)
-	if len(ch.Input.Read(buf)) > 0 {
-		expressionToUse = ch.Input
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(ch.Input)
 
-	} else if len(*inputFile) > 0 {
-		input, err := os.ReadFile(*inputFile)
-
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		expressionToUse = string(input)
-	}
-
-	res, err := Prefix(expressionToUse)
+	res, err := Prefix(buf.String())
 	if err == nil {
-		if len(*outputFile) > 0 {
-			resInString := strconv.Itoa(res)
-			data := []byte(resInString)
+		// 	if len(*outputFile) > 0 {
+		// 		resInString := strconv.Itoa(res)
+		// 		data := []byte(resInString)
 
-			os.WriteFile(*outputFile, data, 0666)
-		} else {
-			fmt.Fprintln(ch.Output, res)
-		}
-	} else {
-		fmt.Fprintln(ch.Output, err)
+		// 		os.WriteFile(*outputFile, data, 0666)
+		// 	} else {
+		// 		fmt.Fprintln(ch.Output, res)
+		// 	}
+		// } else {
+		// 	fmt.Fprintln(ch.Output, err)
 	}
+	fmt.Println(res)
 	return nil
 }
