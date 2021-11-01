3c5c124b06d23c55818abb590431a51c16194d07
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-11-01 12:16:20 +0200

    Update handler_test.go

--- docs/Lab2/handler_test.go
+++ docs/Lab2/handler_test.go
@@ -8,8 +8,9 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func Test1(t *testing.T) {
+func Test_Compute(t *testing.T) {
 	input := "+ 3 2"
+	var expected = "5"
 	inputReader := strings.NewReader(input)
 	output := bytes.NewBufferString("")
 	handler := ComputeHandler{
@@ -17,5 +18,20 @@ func Test1(t *testing.T) {
 		Output: output,
 	}
 	err := handler.Compute()
+	assert.Equal(t, output.String(), expected)
 	assert.Nil(t, err)
+}
+
+func Test_Compute_Error(t *testing.T) {
+	input := "+ 3 2 2"
+	inputReader := strings.NewReader(input)
+	output := bytes.NewBufferString("")
+	handler := ComputeHandler{
+		Input:  inputReader,
+		Output: output,
+	}
+	err := handler.Compute()
+	assert.Equal(t, output.String(), "")
+	assert.NotNil(t, err)
+	// You are durik
 }
