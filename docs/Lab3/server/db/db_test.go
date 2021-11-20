8d54f9bcdf6c0054e03fad39837e29835daf46f9
Dmytrenko-Roman <savitarreus@gmail.com>
2021-11-20 19:49:32 +0200

    ❄️ Added Open test ❄️

--- docs/Lab3/server/db/db_test.go
+++ docs/Lab3/server/db/db_test.go
@@ -1,16 +1,40 @@
 package db
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestDbConnection_ConnectionURL(t *testing.T) {
 	conn := &Connection{
-		DbName:     "db1",
-		User:       "user1",
-		Password:   "pass1",
+		DbName:     "Menu",
+		User:       "postgres",
+		Password:   "hogger",
 		Host:       "localhost",
 		DisableSSL: true,
 	}
-	if conn.ConnectionURL() != "postgres://user1:pass1@localhost/db1?sslmode=disable" {
+	if conn.ConnectionURL() != "postgres://postgres:hogger@localhost/Menu?sslmode=disable" {
 		t.Error("Unexpected connection string")
 	}
+}
+
+func TestDbConnection_Open(t *testing.T) {
+	conn := &Connection{
+		DbName:     "Menu",
+		User:       "postgres",
+		Password:   "hogger",
+		Host:       "localhost",
+		DisableSSL: true,
+	}
+
+	db, err := conn.Open()
+	if err != nil {
+		panic(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		t.Error("Connection failed...")
+	}
+
+	defer db.Close()
 }
