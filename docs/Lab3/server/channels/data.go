bf788229c5964872599f8d1f2967354c2cdac015
Dmytrenko-Roman <savitarreus@gmail.com>
2021-11-21 12:58:36 +0200

    ❄️ Added new functionality ❄️

--- docs/Lab3/server/channels/data.go
+++ docs/Lab3/server/channels/data.go
@@ -2,12 +2,14 @@ package channels
 
 import (
 	"database/sql"
-	"fmt"
+
+	datab "github.com/mezidia/architecture_labs/tree/main/docs/Lab3/server/db"
 )
 
-type Channel struct {
-	Id   int64  `json:"id"`
-	Name string `json:"name"`
+type Dish struct {
+	Id    int64   `json:"id"`
+	Name  string  `json:"name"`
+	Price float32 `json:"price"`
 }
 
 type Store struct {
@@ -18,32 +20,39 @@ func NewStore(db *sql.DB) *Store {
 	return &Store{Db: db}
 }
 
-func (s *Store) ListChannels() ([]*Channel, error) {
-	rows, err := s.Db.Query("SELECT id, name FROM channels LIMIT 200")
+func (s Store) ListMenu() ([]*Dish, error) {
+	conn := &datab.Connection{
+		DbName:     "db_name",
+		User:       "postgres",
+		Password:   "password",
+		Host:       "localhost",
+		DisableSSL: true,
+	}
+	rows, err := conn.SelectAllDishes(s.Db)
 	if err != nil {
 		return nil, err
 	}
 
 	defer rows.Close()
 
-	var res []*Channel
+	var res []*Dish
 	for rows.Next() {
-		var c Channel
+		var c Dish
 		if err := rows.Scan(&c.Id, &c.Name); err != nil {
 			return nil, err
 		}
 		res = append(res, &c)
 	}
 	if res == nil {
-		res = make([]*Channel, 0)
+		res = make([]*Dish, 0)
 	}
 	return res, nil
 }
 
-func (s *Store) CreateChannel(name string) error {
-	if len(name) < 0 {
-		return fmt.Errorf("channel name is not provided")
-	}
-	_, err := s.Db.Exec("INSERT INTO channels (name) VALUES ($1)", name)
-	return err
-}
+// func (s *Store) CreateChannel(name string) error {
+//     if len(name) < 0 {
+//         return fmt.Errorf("channel name is not provided")
+//     }
+//     , err := s.Db.Exec("INSERT INTO channels (name) VALUES ($1)", name)
+//     return err
+// }
