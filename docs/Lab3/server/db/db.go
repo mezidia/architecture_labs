96c7bbb509473765c24e2fc32c77c0662e693eed
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-11-23 10:30:32 +0200

    Update db.go
    Fix SQL syntax

--- docs/Lab3/server/db/db.go
+++ docs/Lab3/server/db/db.go
@@ -34,53 +34,53 @@ func (c *Connection) Open() (*sql.DB, error) {
 }
 
 func InsertOneDish(db *sql.DB, name string, price int) error {
-	_, err := db.Exec("insert into dishes.dishes (name, price) values ($1, $2)",
+	_, err := db.Exec("INSERT INTO dishes (name, price) VALUES ($1, $2)",
 		name, price)
 	return err
 }
 
 func InsertOneOrder(db *sql.DB, dishes []int, table int, suma1 float32, suma2 float32, suma3 float32) error {
-	_, err := db.Exec("insert into orders.orders (dishes, table, suma1, suma2, suma3) values ($1, $2, $3, $4, $5)",
+	_, err := db.Exec("INSERT INTO orders (dishes, table, suma1, suma2, suma3) VALUES ($1, $2, $3, $4, $5)",
 		dishes, table, suma1, suma2, suma3)
 	return err
 }
 
 func SelectAllDishes(db *sql.DB) (*sql.Rows, error) {
-	rows, err := db.Query("select * from dishes")
+	rows, err := db.Query("SELECT * FROM dishes")
 	return rows, err
 }
 
 func SelectAllOrders(db *sql.DB) (*sql.Rows, error) {
-	rows, err := db.Query("select * from orders.orders")
+	rows, err := db.Query("SELECT * FROM orders")
 	return rows, err
 }
 
 func SelectOneDishByID(db *sql.DB, id int) (*sql.Rows, error) {
-	row, err := db.Query("select * from dishes.dishes where id = $1", id)
+	row, err := db.Query("SELECT * FROM dishes WHERE id = $1", id)
 	return row, err
 }
 
 func SelectOneOrderByID(db *sql.DB, id int) (*sql.Rows, error) {
-	row, err := db.Query("select * from orders.orders where id = $1", id)
+	row, err := db.Query("SELECT * FROM orders WHERE id = $1", id)
 	return row, err
 }
 
 func UpdateDish(db *sql.DB, fieldName string, value int, id int) error {
-	_, err := db.Exec("update dishes.dishes set "+fieldName+" = $1 where id = $2", value, id)
+	_, err := db.Exec("UPDATE dishes SET "+fieldName+" = $1 WHERE id = $2", value, id)
 	return err
 }
 
 func UpdateOrder(db *sql.DB, fieldName string, value, id int) error {
-	_, err := db.Exec("update orders.orders set "+fieldName+" = $1 where id = $2", value, id)
+	_, err := db.Exec("UPDATE orders SET "+fieldName+" = $1 WHERE id = $2", value, id)
 	return err
 }
 
 func DeleteDish(db *sql.DB, id int) error {
-	_, err := db.Exec("delete from dishes.dishes where id = $1", id)
+	_, err := db.Exec("DELETE FROM dishes WHERE id = $1", id)
 	return err
 }
 
 func DeleteOrder(db *sql.DB, id int) error {
-	_, err := db.Exec("delete from orders.orders where id = $1", id)
+	_, err := db.Exec("DELETE FROM orders WHERE id = $1", id)
 	return err
 }
