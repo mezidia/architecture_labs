652f9788cc3b778f7f046eb5e41199d1a9c10bee
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-11-28 10:18:27 +0200

    Fix tests
    All tests are checked locally. Some functions in db.go are modified

--- docs/Lab3/server/db/db.go
+++ docs/Lab3/server/db/db.go
@@ -33,10 +33,36 @@ func (c *Connection) Open() (*sql.DB, error) {
 	return sql.Open("postgres", c.ConnectionURL())
 }
 
-func InsertOneOrder(db *sql.DB, id int, table int, dishes []int, sum float64, sumNoVat float64, tip float64) error {
+func SelectAllDishes(db *sql.DB) (*sql.Rows, error) {
+	rows, err := db.Query("SELECT * FROM dishes")
+	return rows, err
+}
 
-	_, err := db.Exec("INSERT INTO orders (id, table_id, dishes, order_sum, sum_no_vat, tip) VALUES ($1, $2, $3, $4, $5, $6)", id, table, pq.Array(dishes), sum, sumNoVat, tip)
+func SelectAllOrders(db *sql.DB) (*sql.Rows, error) {
+	rows, err := db.Query("SELECT * FROM orders")
+	return rows, err
+}
+
+func InsertOneDish(db *sql.DB, name string, price int) error {
+	_, err := db.Exec("INSERT INTO dishes (name, price) VALUES ($1, $2)",
+		name, price)
 	return err
+}
+
+func InsertOneOrder(db *sql.DB, table int, dishes []int, sum float64, sumNoVat float64, tip float64) error {
+
+	_, err := db.Exec("INSERT INTO orders (table_id, dishes, order_sum, sum_no_vat, tip) VALUES ($1, $2, $3, $4, $5)", table, pq.Array(dishes), sum, sumNoVat, tip)
+	return err
+}
+
+func SelectOneDishByID(db *sql.DB, id int) (*sql.Rows, error) {
+	row, err := db.Query("SELECT * FROM dishes WHERE id = $1", id)
+	return row, err
+}
+
+func SelectOneOrderByID(db *sql.DB, id int) (*sql.Rows, error) {
+	row, err := db.Query("SELECT * FROM orders WHERE id = $1", id)
+	return row, err
 }
 
 func SelectOnePriceDishByID(db *sql.DB, id int) *sql.Row {
@@ -44,48 +70,22 @@ func SelectOnePriceDishByID(db *sql.DB, id int) *sql.Row {
 	return row
 }
 
-func SelectAllDishes(db *sql.DB) (*sql.Rows, error) {
-	rows, err := db.Query("SELECT * FROM dishes")
-	return rows, err
+func UpdateDish(db *sql.DB, fieldName string, value int, id int) error {
+	_, err := db.Exec("UPDATE dishes SET "+fieldName+" = $1 WHERE id = $2", value, id)
+	return err
 }
 
-// func InsertOneDish(db *sql.DB, name string, price int) error {
-// 	_, err := db.Exec("INSERT INTO dishes (name, price) VALUES ($1, $2)",
-// 		name, price)
-// 	return err
-// }
+func UpdateOrder(db *sql.DB, fieldName string, value, id int) error {
+	_, err := db.Exec("UPDATE orders SET "+fieldName+" = $1 WHERE id = $2", value, id)
+	return err
+}
 
-// func SelectAllOrders(db *sql.DB) (*sql.Rows, error) {
-// 	rows, err := db.Query("SELECT * FROM orders")
-// 	return rows, err
-// }
+func DeleteDish(db *sql.DB, id int) error {
+	_, err := db.Exec("DELETE FROM dishes WHERE id = $1", id)
+	return err
+}
 
-// func SelectOneDishByID(db *sql.DB, id int) (*sql.Rows, error) {
-// 	row, err := db.Query("SELECT * FROM dishes WHERE id = $1", id)
-// 	return row, err
-// }
-
-// func SelectOneOrderByID(db *sql.DB, id int) (*sql.Rows, error) {
-// 	row, err := db.Query("SELECT * FROM orders WHERE id = $1", id)
-// 	return row, err
-// }
-
-// func UpdateDish(db *sql.DB, fieldName string, value int, id int) error {
-// 	_, err := db.Exec("UPDATE dishes SET "+fieldName+" = $1 WHERE id = $2", value, id)
-// 	return err
-// }
-
-// func UpdateOrder(db *sql.DB, fieldName string, value, id int) error {
-// 	_, err := db.Exec("UPDATE orders SET "+fieldName+" = $1 WHERE id = $2", value, id)
-// 	return err
-// }
-
-// func DeleteDish(db *sql.DB, id int) error {
-// 	_, err := db.Exec("DELETE FROM dishes WHERE id = $1", id)
-// 	return err
-// }
-
-// func DeleteOrder(db *sql.DB, id int) error {
-// 	_, err := db.Exec("DELETE FROM orders WHERE id = $1", id)
-// 	return err
-// }
+func DeleteOrder(db *sql.DB, id int) error {
+	_, err := db.Exec("DELETE FROM orders WHERE id = $1", id)
+	return err
+}
--- docs/Lab3/server/db/db_test.go
+++ docs/Lab3/server/db/db_test.go
@@ -38,185 +38,185 @@ func TestDbConnection_Open(t *testing.T) {
 	}
 }
 
-// func TestDbInsertDish(t *testing.T) {
-// 	conn := &Connection{
-// 		DbName:     "db_name",
-// 		User:       "postgres",
-// 		Password:   "password",
-// 		Host:       "localhost",
-// 		DisableSSL: true,
-// 	}
+func TestDbInsertDish(t *testing.T) {
+	conn := &Connection{
+		DbName:     "Menu",
+		User:       "postgres",
+		Password:   "hogger",
+		Host:       "localhost",
+		DisableSSL: true,
+	}
 
-// 	db, _ := conn.Open()
-// 	err := conn.InsertOneDish(db, "Apple", 72000)
-// 	if err != nil {
-// 		t.Error("Exec failed...")
-// 	}
-// 	defer db.Close()
-// }
+	db, _ := conn.Open()
+	err := InsertOneDish(db, "Apple", 73000)
+	if err != nil {
+		t.Error("Exec failed...")
+	}
+	defer db.Close()
+}
 
-// func TestDbInsertOrder(t *testing.T) {
-// 	conn := &Connection{
-// 		DbName:     "Menu",
-// 		User:       "postgres",
-// 		Password:   "hogger",
-// 		Host:       "localhost",
-// 		DisableSSL: true,
-// 	}
+func TestDbInsertOrder(t *testing.T) {
+	conn := &Connection{
+		DbName:     "Menu",
+		User:       "postgres",
+		Password:   "hogger",
+		Host:       "localhost",
+		DisableSSL: true,
+	}
 
-// 	db, _ := conn.Open()
-// 	err := InsertOneOrder(db, 2, 50, []int{2, 4, 6}, 5.0, 5.0, 5.0)
-// 	if err != nil {
-// 		t.Error("Exec failed...")
-// 	}
-// 	defer db.Close()
-// }
+	db, _ := conn.Open()
+	err := InsertOneOrder(db, 50, []int{2, 4, 6}, 5.0, 5.0, 5.0)
+	if err != nil {
+		t.Error("Exec failed...")
+	}
+	defer db.Close()
+}
 
-// func TestDbSelectOnePriceDishByID(t *testing.T) {
-// 	conn := &Connection{
-// 		DbName:     "Menu",
-// 		User:       "postgres",
-// 		Password:   "hogger",
-// 		Host:       "localhost",
-// 		DisableSSL: true,
-// 	}
+func TestDbSelectAllDishes(t *testing.T) {
+	conn := &Connection{
+		DbName:     "Menu",
+		User:       "postgres",
+		Password:   "hogger",
+		Host:       "localhost",
+		DisableSSL: true,
+	}
 
-// 	db, _ := conn.Open()
-// 	err := SelectOnePriceDishByID(db, 1)
-// 	if err != nil {
-// 		t.Error("Select error...")
-// 	}
-// 	defer db.Close()
-// }
+	db, _ := conn.Open()
+	rows, err := SelectAllDishes(db)
+	if err != nil {
+		t.Error("Exec failed...")
+	}
+	defer rows.Close()
+}
 
-// func TestDbSelectAllDishes(t *testing.T) {
-// 	conn := &Connection{
-// 		DbName:     "db_name",
-// 		User:       "postgres",
-// 		Password:   "password",
-// 		Host:       "localhost",
-// 		DisableSSL: true,
-// 	}
+func TestDbSelectAllOrders(t *testing.T) {
+	conn := &Connection{
+		DbName:     "Menu",
+		User:       "postgres",
+		Password:   "hogger",
+		Host:       "localhost",
+		DisableSSL: true,
+	}
 
-// 	db, _ := conn.Open()
-// 	rows, err := SelectAllDishes(db)
-// 	if err != nil {
-// 		t.Error("Exec failed...")
-// 	}
-// 	defer rows.Close()
-// }
+	db, _ := conn.Open()
+	rows, err := SelectAllOrders(db)
+	if err != nil {
+		t.Error("Exec failed...")
+	}
+	defer rows.Close()
+}
 
-// func TestDbSelectAllOrders(t *testing.T) {
-// 	conn := &Connection{
-// 		DbName:     "db_name",
-// 		User:       "postgres",
-// 		Password:   "password",
-// 		Host:       "localhost",
-// 		DisableSSL: true,
-// 	}
+func TestDbSelectOnePriceDishByID(t *testing.T) {
+	conn := &Connection{
+		DbName:     "Menu",
+		User:       "postgres",
+		Password:   "hogger",
+		Host:       "localhost",
+		DisableSSL: true,
+	}
 
-// 	db, _ := conn.Open()
-// 	rows, err := SelectAllOrders(db)
-// 	if err != nil {
-// 		t.Error("Exec failed...")
-// 	}
-// 	defer rows.Close()
-// }
+	db, _ := conn.Open()
+	row := SelectOnePriceDishByID(db, 1)
+	if row == nil {
+		t.Error("Select error...")
+	}
+	defer db.Close()
+}
 
-// func TestDbSelectOneDish(t *testing.T) {
-// 	conn := &Connection{
-// 		DbName:     "db_name",
-// 		User:       "postgres",
-// 		Password:   "password",
-// 		Host:       "localhost",
-// 		DisableSSL: true,
-// 	}
+func TestDbSelectOneDish(t *testing.T) {
+	conn := &Connection{
+		DbName:     "Menu",
+		User:       "postgres",
+		Password:   "hogger",
+		Host:       "localhost",
+		DisableSSL: true,
+	}
 
-// 	db, _ := conn.Open()
-// 	rows, err := conn.SelectOneDishByID(db, 1)
-// 	if err != nil {
-// 		t.Error("Exec failed...")
-// 	}
-// 	defer rows.Close()
-// }
+	db, _ := conn.Open()
+	rows, err := SelectOneDishByID(db, 1)
+	if err != nil {
+		t.Error("Exec failed...")
+	}
+	defer rows.Close()
+}
 
-// func TestDbSelectOneOrder(t *testing.T) {
-// 	conn := &Connection{
-// 		DbName:     "db_name",
-// 		User:       "postgres",
-// 		Password:   "password",
-// 		Host:       "localhost",
-// 		DisableSSL: true,
-// 	}
+func TestDbSelectOneOrder(t *testing.T) {
+	conn := &Connection{
+		DbName:     "Menu",
+		User:       "postgres",
+		Password:   "hogger",
+		Host:       "localhost",
+		DisableSSL: true,
+	}
 
-// 	db, _ := conn.Open()
-// 	rows, err := conn.SelectOneOrderByID(db, 1)
-// 	if err != nil {
-// 		t.Error("Exec failed...")
-// 	}
-// 	defer rows.Close()
-// }
+	db, _ := conn.Open()
+	rows, err := SelectOneOrderByID(db, 1)
+	if err != nil {
+		t.Error("Exec failed...")
+	}
+	defer rows.Close()
+}
 
-// func TestDbUpdateDish(t *testing.T) {
-// 	conn := &Connection{
-// 		DbName:     "db_name",
-// 		User:       "postgres",
-// 		Password:   "password",
-// 		Host:       "localhost",
-// 		DisableSSL: true,
-// 	}
+func TestDbUpdateDish(t *testing.T) {
+	conn := &Connection{
+		DbName:     "Menu",
+		User:       "postgres",
+		Password:   "hogger",
+		Host:       "localhost",
+		DisableSSL: true,
+	}
 
-// 	db, _ := conn.Open()
-// 	err := conn.UpdateDish(db, "price", 655000, 1)
-// 	if err != nil {
-// 		t.Error("Exec failed...")
-// 	}
-// }
+	db, _ := conn.Open()
+	err := UpdateDish(db, "price", 655000, 1)
+	if err != nil {
+		t.Error("Exec failed...")
+	}
+}
 
-// func TestDbUpdateOrder(t *testing.T) {
-// 	conn := &Connection{
-// 		DbName:     "db_name",
-// 		User:       "postgres",
-// 		Password:   "password",
-// 		Host:       "localhost",
-// 		DisableSSL: true,
-// 	}
+func TestDbUpdateOrder(t *testing.T) {
+	conn := &Connection{
+		DbName:     "Menu",
+		User:       "postgres",
+		Password:   "hogger",
+		Host:       "localhost",
+		DisableSSL: true,
+	}
 
-// 	db, _ := conn.Open()
-// 	err := conn.UpdateOrder(db, "table", 67000, 1)
-// 	if err != nil {
-// 		t.Error("Exec failed...")
-// 	}
-// }
+	db, _ := conn.Open()
+	err := UpdateOrder(db, "table_id", 67000, 1)
+	if err != nil {
+		t.Error("Exec failed...")
+	}
+}
 
-// func TestDbDeleteDish(t *testing.T) {
-// 	conn := &Connection{
-// 		DbName:     "db_name",
-// 		User:       "postgres",
-// 		Password:   "password",
-// 		Host:       "localhost",
-// 		DisableSSL: true,
-// 	}
+func TestDbDeleteDish(t *testing.T) {
+	conn := &Connection{
+		DbName:     "Menu",
+		User:       "postgres",
+		Password:   "hogger",
+		Host:       "localhost",
+		DisableSSL: true,
+	}
 
-// 	db, _ := conn.Open()
-// 	err := conn.DeleteDish(db, 1)
-// 	if err != nil {
-// 		t.Error("Exec failed...")
-// 	}
-// }
+	db, _ := conn.Open()
+	err := DeleteDish(db, 1)
+	if err != nil {
+		t.Error("Exec failed...")
+	}
+}
 
-// func TestDbDeleteOrder(t *testing.T) {
-// 	conn := &Connection{
-// 		DbName:     "db_name",
-// 		User:       "postgres",
-// 		Password:   "password",
-// 		Host:       "localhost",
-// 		DisableSSL: true,
-// 	}
+func TestDbDeleteOrder(t *testing.T) {
+	conn := &Connection{
+		DbName:     "Menu",
+		User:       "postgres",
+		Password:   "hogger",
+		Host:       "localhost",
+		DisableSSL: true,
+	}
 
-// 	db, _ := conn.Open()
-// 	err := conn.DeleteOrder(db, 1)
-// 	if err != nil {
-// 		t.Error("Exec failed...")
-// 	}
-// }
+	db, _ := conn.Open()
+	err := DeleteOrder(db, 1)
+	if err != nil {
+		t.Error("Exec failed...")
+	}
+}
