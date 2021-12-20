cff2fe35476645e0ea008de0a2ea1e98936704bd
Valentyn Dominskyi <Valentyn2202@gmail.com>
2021-12-20 22:25:36 +0200

    🚧: add first working implementation of benchmark test

--- docs/Lab2/bench_test.go
+++ docs/Lab2/bench_test.go
@@ -2,22 +2,27 @@ package lab2
 
 import (
 	"fmt"
-	"math/rand"
 	"testing"
 )
 
 var cntRes int
+var input string
+var err error
 
-func BenchmarkCount(b *testing.B) {
-	const baseLen = 5
-	for i := 0; i < 20; i++ {
-		l := baseLen * 1 << i
-		in := make([]int, l)
-		for k := 0; k < l; k++ {
-			in[k] = rand.Intn(10)
+func BenchmarkPrefix(b *testing.B) {
+	l := 0
+	expr := "+ - / * 1 2 3 4 5"
+	for k := 0; k < 20; k++ {
+		input = expr
+		l += 2000
+		for i := 0; i < l; i++ {
+			input = "+ " + input
+			input = input + " "
+			input = input + expr
 		}
+
 		b.Run(fmt.Sprintf("len=%d", l), func(b *testing.B) {
-			cntRes = Count(make([]int, l), 0)
+			cntRes, err = Prefix(input)
 		})
 	}
 }
