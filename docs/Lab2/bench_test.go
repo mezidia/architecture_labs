ff6398cb69ab69e6746b71144f53dd8b79d40141
Valentyn Dominskyi <Valentyn2202@gmail.com>
2021-12-20 23:02:33 +0200

    🚧: add second working implementation of benchmark test

--- docs/Lab2/bench_test.go
+++ docs/Lab2/bench_test.go
@@ -5,24 +5,30 @@ import (
 	"testing"
 )
 
-var cntRes int
-var input string
-var err error
+var (
+	startStatement = "+ - / * 1 2 3 4 5"
+)
 
 func BenchmarkPrefix(b *testing.B) {
-	l := 0
-	expr := "+ - / * 1 2 3 4 5"
+	iterationsNum := 0
+
 	for k := 0; k < 20; k++ {
-		input = expr
-		l += 2000
-		for i := 0; i < l; i++ {
+		input := startStatement
+		iterationsNum += 2000
+
+		for i := 0; i < iterationsNum; i++ {
 			input = "+ " + input
 			input = input + " "
-			input = input + expr
+			input = input + startStatement
 		}
 
-		b.Run(fmt.Sprintf("len=%d", l), func(b *testing.B) {
-			cntRes, err = Prefix(input)
+		b.Run(fmt.Sprintf("len=%d", iterationsNum), func(b *testing.B) {
+			cntRes, err := Prefix(input)
+
+			if err != nil {
+				fmt.Println(err)
+				fmt.Println("Result is ", cntRes)
+			}
 		})
 	}
 }
